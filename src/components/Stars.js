ef9406f7b817675d4ec1ccd70fc19c0db495231b
Nedrath <Mark.P.Eriksen@gmail.com>
2020-11-10 10:28:11 +0100

    Stars done indtil API

---
+++ src/components/Stars.js
@@ -0,0 +1,21 @@
+import { css } from "emotion";
+import GoldStar from '../img/goldStar.svg'
+import EmptyStar from '../img/emptyStar.svg'
+
+const Stars = () => {
+  const styleSection = css`
+    display: flex;
+    justify-content: space-between;
+    width: 93px;
+  `;
+
+  return <section className={styleSection}>
+    <img src={GoldStar} alt=""/>
+    <img src={GoldStar} alt=""/>
+    <img src={EmptyStar} alt=""/>
+    <img src={EmptyStar} alt=""/>
+    <img src={EmptyStar} alt=""/>
+  </section>;
+};
+
+export default Stars;
