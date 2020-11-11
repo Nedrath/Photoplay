6f4944ab1f6e4802e31ec7d2c7147a8d310557be
Ned <6065203@RTS-365.dk>
2020-11-11 14:09:54 +0100

    nav/footer og navigation done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,20 +1,45 @@
 import { css } from "emotion";
-import ImageX from "../img/ImageFrontPage.svg";
-import ShadowTop from "./ShadowTop";
-import ShadowBottom from "./ShadowBottom";
+import Navigation from './Navigation'
+import iconHome from '../img/Icons-Navigation/Home icon.svg'
+import iconSearch from '../img/Icons-Navigation/search-2.svg'
+import iconDownloads from '../img/Icons-Navigation/download.svg'
+import iconProfile from '../img/Icons-Navigation/man-user.svg'
 
 const Footer = () => {
-  const styleFooter = css`
-    height: 540px;
-    margin: auto;
-    overflow: hidden;
+    const styleNav = css`
+    height: 79px;
+    width: 100%;
+    position: fixed;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    padding-top: 15px;
+    padding-bottom: 28px;
+    padding-left: 40px;
+    padding-right: 40px;
+    background: #1A1A1A;
+    display: flex;
+    justify-content: space-between;
   `;
   return (
-    <footer className={styleFooter}>
-      <ShadowTop />
-      <img src={ImageX} alt="" />
-      <ShadowBottom />
-    </footer>
+    <nav className={styleNav}>
+     <Navigation 
+     icon={iconHome}
+     text="home"
+     />
+     <Navigation 
+     icon={iconSearch}
+     text="search"
+     />
+     <Navigation 
+     icon={iconDownloads}
+     text="downloads"
+     />
+     <Navigation 
+     icon={iconProfile}
+     text="profile"
+     />
+    </nav>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,12 @@
+import { css } from "emotion";
 
 const Header = () => {
-  return (
-    <header>
-
-      
-    </header>
-  );
+  const styleWrapper = css`
+    height: 540px;
+    margin: auto;
+    overflow: hidden;
+  `;
+  return <header className={styleWrapper}></header>;
 };
 
 export default Header;
