c63aa2a0621c7688e0435317b583e53ae318f31d
Ned <6065203@RTS-365.dk>
2020-11-12 10:06:52 +0100

    Chevron done mangler back useEffect (window.location.back)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Router } from "@reach/router";
 import { render } from 'react-dom'
 import Home from './views/Home'
+// import MovieDetails from './views/MovieDetails'
 // import UserContextProvider from "./context/UserContext";
 
 const App = () => {
   return (
     <Router>
       <Home path="/" />
+      {/* <MovieDetails path="/MovieDetails" /> */}
     </Router>
   );
 };
