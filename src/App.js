b41822e72406ca507a5585df2061bd1f4c907d28
Ned <6065203@RTS-365.dk>
2020-11-12 12:41:38 +0100

    igang med movie details page, mangler 2 komponenter jeg har glemt at lave

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { Router } from "@reach/router";
 import { render } from 'react-dom'
 import Home from './views/Home'
-// import MovieDetails from './views/MovieDetails'
+import MovieDetails from './views/MovieDetails'
 // import UserContextProvider from "./context/UserContext";
 
 const App = () => {
   return (
     <Router>
       <Home path="/" />
-      {/* <MovieDetails path="/MovieDetails" /> */}
+      <MovieDetails path="/MovieDetails" />
     </Router>
   );
 };
