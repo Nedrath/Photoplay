37f610399790b6fec91f05b229be1c0ef6918b23
Ned <6065203@RTS-365.dk>
2020-11-13 12:28:42 +0100

    samling done

---
+++ src/components/Rating-Stars.js
@@ -0,0 +1,37 @@
+import { css } from "emotion";
+import GoldStar from '../img/goldStar.svg'
+import EmptyStar from '../img/emptyStar.svg'
+
+const RatingStars = () => {
+  const styleText = css`
+    display: flex;
+    position: relative;
+    justify-content: center;
+    padding: 5px 0px;
+    font-size: 33px;
+    font-weight: 500;
+    color: white;
+    z-index: 87;
+  `;
+  const styleSection = css`
+    display: flex;
+    justify-content: space-between;
+    margin: auto;
+    width: 93px;
+  `;
+
+  return (
+    <div>
+      <p className={styleText}>4.0</p>
+      <section className={styleSection}>
+        <img src={GoldStar} alt="" />
+        <img src={GoldStar} alt="" />
+        <img src={EmptyStar} alt="" />
+        <img src={EmptyStar} alt="" />
+        <img src={EmptyStar} alt="" />
+      </section>
+    </div>
+  );
+};
+
+export default RatingStars;
