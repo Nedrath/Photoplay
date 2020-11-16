cbda0c2552f6fcec68c9e200c1276ef0f2c3d962
Ned <6065203@RTS-365.dk>
2020-11-16 10:52:28 +0100

    done mangler API + search function skal kunne virke i SearchBar

--- src/components/DividerCategorie.js
+++ src/components/DividerCategorie.js
@@ -1,24 +1,30 @@
 import { css } from "emotion";
-import Divider from '../img/Divider.svg'
+import Divider from "../img/Divider.svg";
 
-const DividerCategorie = ({ text }) => {
+const DividerCategorie = ({ text, dividerWidth }) => {
   const styleSection = css`
-  height: 16px;
+    height: 16px;
+    width: 340px;
+    display: flex;
+    /* justify-content: space-evenly; */
   `;
   const styleText = css`
     font-size: 12px;
     font-weight: 400;
     color: white;
     text-transform: uppercase;
+    text-align: left;
   `;
   const styleDivider = css`
     padding-bottom: 3px;
-    padding-left: 15px;
-  `
+    width: ${dividerWidth};
+    padding-left: 20px;
+  `;
 
   return (
     <section className={styleSection}>
-      <p className={styleText}>{text} <span><img className={styleDivider} src={Divider} alt=""/></span> </p>
+      <p className={styleText}>{text}</p>
+      <img className={styleDivider} src={Divider} alt="" />
     </section>
   );
 };
